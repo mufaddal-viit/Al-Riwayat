91802c3a1735b0a98e957019cdf82621c3cbf75c
mufaddal_viit <mufaddal.21810212@viit.ac.in>
2026-06-01 08:05:33 +0530

    date change

--- web/lib/content/issues/issue-1.ts
+++ web/lib/content/issues/issue-1.ts
@@ -6,7 +6,7 @@ const title = "The Quiet Return Of Intentional Reading";
 export const issueOne: IssueContent = {
   title,
   issueNumber: 1,
-  publishedAt: "2026-01-15T09:00:00.000Z",
+  publishedAt: "2026-03-19T09:00:00.000Z",
   author: "Editorial Desk",
   slug: slugify(title),
   flipbookUrl: "https://heyzine.com/flip-book/7447c8853d.html",
